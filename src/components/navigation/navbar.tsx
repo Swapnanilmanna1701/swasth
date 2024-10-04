9230be8ab11416b7b9e87dc520d90f353dc356a0
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-10-04 13:06:33 +0530

    changes

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -4,46 +4,66 @@ import Icons from "../global/icons";
 import MaxWidthWrapper from "../global/max-width-wrapper";
 import { buttonVariants } from "../ui/button";
 import UserAccount from "../user-account";
+import Image from "next/image";
 
 interface Props {
-    user: User | null;
+  user: User | null;
 }
 
 const Navbar = ({ user }: Props) => {
-    return (
-        <header className="sticky top-0 inset-x-0 w-full h-14 border-b border-border/40 bg-background/50 backdrop-blur-md z-50">
-            <MaxWidthWrapper>
-                <div className="flex items-center justify-between w-full h-full">
-                    <div className="flex">
-                        <Link href="/" className="flex items-center font-semibold gap-2 text-lg">
-                            <Icons.logo className="w-8 h-8" />
-                            Swasth
-                        </Link>
-                    </div>
+  return (
+    <header className="sticky top-0 inset-x-0 w-full h-18 border-b border-border/40 bg-background/50 backdrop-blur-md z-50">
+      <MaxWidthWrapper>
+        <div className="flex justify-between w-full h-full">
+          <div className="flex">
+            <Link
+              href="/"
+              className="flex items-center font-semibold gap-1 text-lg"
+            >
+              <Image
+                src="/icons/logo.png"
+                width={80}
+                height={80}
+                alt="Swasth Logo"
+                className="object-contain w-auto
+                 h-22 transition-all duration-700 ease-out dark:hidden"
+              />
+              Swasth
+            </Link>
+          </div>
 
-                    <div className="flex items-center gap-4">
-                        {user ? (
-                            <>
-                                <Link href="/dashboard" className={buttonVariants({ size: "sm" })}>
-                                    Dashboard
-                                </Link>
-                                <UserAccount />
-                            </>
-                        ) : (
-                            <>
-                                <Link href="/auth/signin" className={buttonVariants({ size: "sm", variant: "ghost" })}>
-                                    Login
-                                </Link>
-                                <Link href="/auth/signup" className={buttonVariants({ size: "sm" })}>
-                                    Start for free
-                                </Link>
-                            </>
-                        )}
-                    </div>
-                </div>
-            </MaxWidthWrapper>
-        </header>
-    )
+          <div className="flex items-center gap-4">
+            {user ? (
+              <>
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({ size: "sm" })}
+                >
+                  Dashboard
+                </Link>
+                <UserAccount />
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/auth/signin"
+                  className={buttonVariants({ size: "sm", variant: "ghost" })}
+                >
+                  Login
+                </Link>
+                <Link
+                  href="/auth/signup"
+                  className={buttonVariants({ size: "sm" })}
+                >
+                  Start for free
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </header>
+  );
 };
 
-export default Navbar
+export default Navbar;
